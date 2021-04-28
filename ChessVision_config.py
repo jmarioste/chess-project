fb764d49c488180e9fdc2dbd6af5584a08ef059e
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2021-04-28 08:48:00 +0800

    Fix token expiry on lambda

--- ChessVision_config.py
+++ ChessVision_config.py
@@ -3,3 +3,4 @@ from botocore.config import Config
 my_config = Config(
     region_name = 'eu-central-1'
 )
+function_name='arn:aws:lambda:eu-central-1:580857158266:function:chessvisionClient'
--- ChessVision_wrapper.py
+++ ChessVision_wrapper.py
@@ -14,29 +14,30 @@ from botocore.config import Config
 import utils
 import ChessVision_config as conf
 
-    # AWS.config.region = 'eu-central-1'; // Region
-    # AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    #     IdentityPoolId: 'eu-central-1:d06d1df9-443e-49e3-84e8-d90aacb9b333',
-    # });
 
-    # lambda = new AWS.Lambda({region: "eu-central-1"});
-    # lambda.config.credentials = AWS.config.credentials;
-    # lambda.config.region = AWS.config.region;
+def inititalize_credentials():
+  client = boto3.client('cognito-identity', config=conf.my_config)
+  
+  identity_id = client.get_id(
+    IdentityPoolId='eu-central-1:d06d1df9-443e-49e3-84e8-d90aacb9b333'
+  )['IdentityId']
+  response = client.get_credentials_for_identity(
+    IdentityId=identity_id
+  )
+  credentials = response['Credentials']
+  return credentials
 
-    # lambda.config.credentials.get(function(){
-    #     var accessKeyId = AWS.config.credentials.accessKeyId;
-    #     var secretAccessKey = AWS.config.credentials.secretAccessKey;
-    #     var sessionToken = AWS.config.credentials.sessionToken;
-    # });
 
-client = boto3.client('lambda',
-  aws_access_key_id=conf.aws_access_key_id,
-  aws_secret_access_key=conf.aws_secret_access_key,
-  aws_session_token=conf.aws_session_token
+def invoke_lambda(credentials, json_payload):
+  client = boto3.client('cognito-identity', config=conf.my_config)
+  
+  lambda_client = boto3.client('lambda',
+  aws_access_key_id=credentials['AccessKeyId'],
+  aws_secret_access_key=credentials['SecretKey'],
+  aws_session_token=credentials['SessionToken']
 , config = conf.my_config)
 
-def invoke_lambda(json_payload):
-  response = client.invoke(
+  response = lambda_client.invoke(
       FunctionName=conf.function_name,
       InvocationType='RequestResponse',
       Payload=json_payload
@@ -60,7 +61,7 @@ def get_json_from_img(path):
     'flip': "false"
   })
 
-def get_fen_from_dir(chessboards_dir):
+def get_fen_from_dir(credentials, chessboards_dir):
   # chessboards_dir = '/mnt/g/Documents/Chess/1001 Chess Exercises for Club Players/output/cb_sample_300/'
   chessboard_images = utils.get_files_from_dir(chessboards_dir)
   
@@ -68,7 +69,7 @@ def get_fen_from_dir(chessboards_dir):
   
   for img_path in chessboard_images:
     json_data = get_json_from_img(path.join(chessboards_dir, img_path))
-    response_data = invoke_lambda(json_data)
+    response_data = invoke_lambda(credentials, json_data)
     print(response_data['FEN'])
   
 def sample():
@@ -78,6 +79,8 @@ def sample():
 
 if __name__ == '__main__':
   start = time.time()
-  get_fen_from_dir('/mnt/g/Documents/Chess/1001 Chess Exercises for Club Players/output/chessboard/')
+  credentials = inititalize_credentials()
+  get_fen_from_dir(credentials, '/mnt/g/Documents/Chess/1001 Chess Exercises for Club Players/output/chessboard/')
+  
   end = time.time()
   print('execution time: %ds', end-start)
